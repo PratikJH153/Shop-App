ad74ff05d513bf73c96aaff9cf3ad83620118d9c
PratikJH <pratik.jh2017@gmail.com>
2021-12-21 13:37:46 +0530

    Added Confirm Dismiss and Alert Dialog Functionality

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -21,6 +21,30 @@ class CartItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Dismissible(
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: const Text("Are you sure?"),
+            content:
+                const Text("Do you want to remove the item from the cart?"),
+            actions: [
+              TextButton(
+                child: const Text("No"),
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+              ),
+              TextButton(
+                child: const Text("Yes"),
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+              ),
+            ],
+          ),
+        );
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productID);
       },
